1bed34bd1e2b9783a06bfa910ce1a1b8fc212301
Adam Brunn <adambrunn1@hotmail.com>
2017-04-28 21:34:48 -0500

    Finished Requirements

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("P.Terry's Double Cheese Burger", false), ("Mushroom Swiss Burger", false), ("Applewood Smoked Bacon Burger", false);
